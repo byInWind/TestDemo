5e20c58fa69e725005a9a1001635e24a0cbd565f
lihao <lihao.cuapp.me>
2018-07-25 17:44:23 +0800

    引入了插件plugin，管理输出

---
+++ webpack-demo/test4/readme.md
@@ -0,0 +1,5 @@
+run:   npm i  
+run:   npm run bulid 
+看到Hello webpack，加上里背景色与图片，运行成功  
+前题：有node和webpack4.0以上环境  
+引入了插件plugin，管理输出
---
+++ webpack-demo/test4/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+//没看懂的插件
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//通常，在每次构建前清理 /dist 文件夹，只会生成用到的文件,其它文件删除
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: {
+        //入口变成两个
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: '输出 管理'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,
+                use: [
+                    //在js里引入css的依赖 npm install --save-dev style-loader css-loader
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpg|png|gif|svg|webp)$/,
+                use: [
+                    //在js里引入图片的依赖（在js里引入的css里引入的图片也需要这样处理） npm install --save-dev file-loader
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+}
