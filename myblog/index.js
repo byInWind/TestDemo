1cefb05c45db0d765b12737fd8c5382412a26321
lihao <lihao.cuapp.me>
2019-01-09 11:09:06 +0800

    登录页展示

--- myblog/index.js
+++ myblog/index.js
@@ -5,23 +5,10 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 const config = require('config-lite')(__dirname)
 const routes = require('./routes')
-const pkg = require('./package');
+const pkg = require('./package')
 
-const app = express();
+const app = express()
 
-// 设置模板全局常量
-app.locals.blog = {
-    title: pkg.name,
-    description: pkg.description
-};
-
-// 添加模板必需的三个变量
-app.use(function (req, res, next) {
-    res.locals.user = req.session.user
-    res.locals.success = req.flash('success').toString()
-    res.locals.error = req.flash('error').toString()
-    next()
-});
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'))
 // 设置模板引擎为 ejs
@@ -45,6 +32,19 @@ app.use(session({
 // flash 中间件，用来显示通知
 app.use(flash())
 
+// 设置模板全局常量
+app.locals.blog = {
+    title: pkg.name,
+    description: pkg.description
+}
+
+// 添加模板必需的三个变量
+app.use(function (req, res, next) {
+    res.locals.user = req.session.user
+    res.locals.success = req.flash('success').toString()
+    res.locals.error = req.flash('error').toString()
+    next()
+})
 // 路由
 routes(app)
 
--- myblog/routes/login.js
+++ myblog/routes/login.js
@@ -5,7 +5,7 @@ const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 // GET /signup 注册页
 router.get('/', checkNotLogin, function (req, res, next) {
-    res.send('注册页')
+    res.render('login')
 })
 
 // POST /signup 用户注册
